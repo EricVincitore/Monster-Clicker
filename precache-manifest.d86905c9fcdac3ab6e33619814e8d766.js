2ea0676a866826520eb53fd4f550818c4a78a13e
EricVincitore <epv1001@verizon.net>
2020-04-15 13:16:11 -0400

    Updates

---
+++ precache-manifest.d86905c9fcdac3ab6e33619814e8d766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "841828a4784d15ec66e9bd0660c9371a",
+    "url": "/Monster-Clicker/index.html"
+  },
+  {
+    "revision": "9588dae4cde3cdadedfa",
+    "url": "/Monster-Clicker/static/css/main.8a624006.chunk.css"
+  },
+  {
+    "revision": "7fd6449284166590bb2b",
+    "url": "/Monster-Clicker/static/js/2.d40cf16c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Clicker/static/js/2.d40cf16c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9588dae4cde3cdadedfa",
+    "url": "/Monster-Clicker/static/js/main.50b8526c.chunk.js"
+  },
+  {
+    "revision": "ed5fd8f68ed3a14d0d9b",
+    "url": "/Monster-Clicker/static/js/runtime-main.87362cd0.js"
+  }
+]);
